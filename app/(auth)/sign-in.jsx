96ee22881643791ab8d800138b685a3b3ae0d862
hmongdev <steveyangwork@gmail.com>
2024-05-14 11:45:59 -0500

    create sign-in and sign-up components

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -5,11 +5,20 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import FormField from '../../components/FormField';
 import { images } from '../../constants';
 
+import { Link } from 'expo-router';
+import CustomButton from '../../components/CustomButton';
+
 const SignIn = () => {
+	const [isSubmitting, setIsSubmitting] = useState(false);
 	const [form, setForm] = useState({
 		email: '',
 		password: '',
 	});
+
+	// submit fn
+	const submit = () => {
+		setIsSubmitting(true);
+	};
 
 	return (
 		<SafeAreaView className="bg-primary h-full">
@@ -43,6 +52,33 @@ const SignIn = () => {
 						otherStyles="mt-7"
 						keyboardType="email-address"
 					/>
+					<FormField
+						title="Password"
+						value={form.password}
+						handleChangeText={(e) =>
+							setForm({
+								...form,
+								password: e,
+							})
+						}
+						otherStyles="mt-7"
+					/>
+					<CustomButton
+						title="Sign In"
+						handlePress={submit}
+						containerStyles="mt-7"
+						isLoading={isSubmitting}
+					/>
+					<View className="flex justify-center pt-5 flex-row gap-2">
+						<Text className="text-lg text-gray-100 font-pregular">
+							Don't have an account?
+						</Text>
+						<Link
+							href="/sign-up"
+							className="text-lg font-psemibold text-secondary">
+							Signup
+						</Link>
+					</View>
 				</View>
 			</ScrollView>
 		</SafeAreaView>
